af85d64840e833737f6e52115f62d51326d6df9c
Jorge arana <koki.arana.reyes@gmail.com>
2020-10-01 20:20:04 -0500

    docs agregado

---
+++ docs/precache-manifest.ab94537e8cc7ce52dc3ea5323bb1b8f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6bf523728b4cc602202ef922eae9a61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35ce74a6f5d1a423ef91",
+    "url": "/static/css/main.2e60803d.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35ce74a6f5d1a423ef91",
+    "url": "/static/js/main.859a5b05.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
