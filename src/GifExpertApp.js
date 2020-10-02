d14c6f19d1d48ca979bb311985dd0bf88b0a73d3
Jorge arana <koki.arana.reyes@gmail.com>
2020-10-01 20:17:41 -0500

    first commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+    
+    const [categories, setCategories] = useState(['One Punch']);
+
+    // const handleAdd = () => {
+    //     setCategories(['HunterxHunter',...categories] );
+    // }
+
+
+    return (
+        <div>
+            <h2>GiftExpertApp</h2>
+            <AddCategory setCategories={ setCategories }/>
+            <hr />
+
+            
+
+            <ol>
+                { 
+                    categories.map( category =>
+                      <GifGrid 
+                      key={ category }
+                      category={ category } 
+                      />
+                    )  
+                }
+            </ol>
+
+        </div>
+    )
+}
